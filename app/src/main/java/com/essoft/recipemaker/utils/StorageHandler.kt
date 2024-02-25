808b26fbf5105744f6232180ed093dbb69af228c
eugene <es-jliew@gmail.com>
2024-02-26 04:24:40 +0800

    Fix internal storage issue when copying file using local uri

--- app/src/main/java/com/essoft/recipemaker/utils/StorageHandler.kt
+++ app/src/main/java/com/essoft/recipemaker/utils/StorageHandler.kt
@@ -3,20 +3,41 @@ package com.essoft.recipemaker.utils
 import android.content.Context
 import android.net.Uri
 import android.provider.OpenableColumns
+import android.util.Log
+import com.essoft.recipemaker.constants.FileScheme
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 
 class StorageHandler (private val context: Context): IStorageHandler {
     override fun copyFileByUri(pathFrom: Uri): String {
-        val inputStream = context.contentResolver.openInputStream(pathFrom)
-        val destFile = File(context.getExternalFilesDir(null), getFileNameByUri(pathFrom, context).toString())
-        val output = FileOutputStream(destFile)
-        inputStream?.copyTo(output, 4 * 1024)
-        inputStream?.close()
-        return destFile.toURI().toString()
+        if (isLocalFile(pathFrom)) {
+            val inputStream = FileInputStream(File(pathFrom.path.toString()))
+            val destFile = File(context.filesDir, getFileNameByInternalUri(pathFrom)) // Or any other destination path
+            val output = FileOutputStream(destFile)
+
+            inputStream.copyTo(output, 4 * 1024)
+            inputStream.close()
+            output.close() // Close output stream as well
+
+            return destFile.toURI().toString()
+        } else {
+            val inputStream = context.contentResolver.openInputStream(pathFrom)
+            val destFile = File(context.getExternalFilesDir(null), getFileNameByExternalUri(pathFrom, context).toString())
+            val output = FileOutputStream(destFile)
+
+            inputStream?.copyTo(output, 4 * 1024)
+            inputStream?.close()
+            output.close() // Close output stream as well
+            return destFile.toURI().toString()
+        }
     }
 
-    private fun getFileNameByUri(uri: Uri, context: Context): String? {
+    private fun isLocalFile(uri: Uri): Boolean {
+        return uri.scheme.equals(FileScheme.FILE)
+    }
+
+    private fun getFileNameByExternalUri(uri: Uri, context: Context): String? {
         val cursor = context.contentResolver.query(uri, null, null, null, null)
         return cursor.use { it ->
             if (it != null) {
@@ -25,5 +46,9 @@ class StorageHandler (private val context: Context): IStorageHandler {
                 it.getString(nameIndex)
             } else ""
         }
+    }
+
+    private fun getFileNameByInternalUri(uri: Uri): String {
+        return uri.toString().split("/").last()
     }
 }
